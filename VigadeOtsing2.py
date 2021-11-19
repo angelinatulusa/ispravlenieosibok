5990fc7e85d3d122884ca933b1bb2133bb1c95e3
opilane <opilane@HP-CZC6297BB4>
2021-11-19 09:48:24 +0200

    ispravila osnovnie osibki, no poka vse ravno rabotaet ne tak

--- VigadeOtsing2.py
+++ VigadeOtsing2.py
@@ -3,7 +3,7 @@ print()
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 while 1:
     try:
-        a = (abs(int(input("Введите целое число => "))
+        a=abs(int(input("Введите целое число => ")))#dobavit ese )
         break
     except ValueError:
          print("Это не целое число")
@@ -12,44 +12,46 @@ if a==0:
     print("Нет смысла ничего делать с нулём")
 else:
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    b=0 #dobavit peremennie b i c
+    c=0
     print("Определяем, сколько в числе чётных и сколько нечётных цифр")
     print()
-    c==b==a
-    paaris ==0
-    paaritu == 0
-    while b > 0;
-            if b % 2 = 0:
-                    paaris =+ 1
-            else:
-                    paaritu =+ 1
-             b = b // 10
+    c=b=a
+    paaris=0
+    paaritu=0
+    while b > 0:#вместо ; надо использовать :
+        if b % 2==0: #if надо правильно подставить под while и дописать еще одно =
+            paaris += 1#nado pravolno podstavit pod if
+        else: #else nado pravilno podstavit pod while
+            paaritu += 1#nado pravolno podstavit pod else
+            b = b // 10
     
-    print("Чётных цифр:"paaris)
-    print("Нечётных цифр:"paaritu)
+    print("Чётных цифр:",paaris)#posle teksta postavit ,
+    print("Нечётных цифр:",paaritu)
     print()
 #''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     print("*Переворачиваем* введённое число")
     print()
     b=0
-    while a > 0
+    while a > 0:#posle while nado ispolzovat :
         number = a % 10
         a = a // 10
         b = b * 10
-         b =+ number
+        b =+ number#pravilno podstavit pod while
     print("*Перевёрнутое* число", b)
     print()
 #''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    print(("Проверяем гипотезу Сиракуз")
+    print("Проверяем гипотезу Сиракуз")#lisnaja (
     print()
-    if c % 2 = 0:
+    if c % 2 == 0:#dobavit ese odno =
         print("с - чётное число. Делим на 2.")
     else:
         print("с - нечётное число. Умножаем на 3, прибавляем 1 и делим на 2.")
     while c != 1:
-            if c % 2 = 0:
-                    c == c / 2
-            else:
-                    c == (3*c + 1) / 2
-            print(c, end=")
+        if c % 2 == 0:#dobavit ese odno =
+            c == c / 2
+        else:
+            c == (3*c + 1) / 2
+            print(c, end=" ")#zakrit "
     print()
-    print("Гипотеза верна'')
+    print("Гипотеза верна")#ne te " bili ispolzovani
