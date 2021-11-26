7269ab4fb42057b68237e2e5545fac48f5c5f700
opilane <opilane@HP-CZC6297BB4>
2021-11-26 09:49:26 +0200

    osibki ispravleni

--- VigadeOtsing2.py
+++ VigadeOtsing2.py
@@ -2,56 +2,54 @@ print("*** ИГРЫ С ЧИСЛАМИ ***")
 print()
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 while 1:
-    try:
-        a=abs(int(input("Введите целое число => ")))#dobavit ese )
+    try:#proverka celoe li cislo,esli net,to sprasivaet ese raz
+        a=abs(int(input("Введите целое число => ")))#abs-vozvrasaet modul cisla
         break
     except ValueError:
          print("Это не целое число")
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-if a==0:
+if a==0:#proveraet ravno li zadannoe cislo 0 i esli da,to ni4ego c nim ne delaet
     print("Нет смысла ничего делать с нулём")
 else:
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    b=0 #dobavit peremennie b i c
-    c=0
     print("Определяем, сколько в числе чётных и сколько нечётных цифр")
     print()
     c=b=a
-    paaris=0
+    paaris=0 #dobavit peremennie paaris i paaritu
     paaritu=0
-    while b > 0:#вместо ; надо использовать :
-        if b % 2==0: #if надо правильно подставить под while и дописать еще одно =
-            paaris += 1#nado pravolno podstavit pod if
-        else: #else nado pravilno podstavit pod while
-            paaritu += 1#nado pravolno podstavit pod else
-            b = b // 10
+    while b > 0:
+        if b % 2==0: 
+            paaris+=1
+        else: 
+            paaritu+=1
+        b=b//10
     
-    print("Чётных цифр:",paaris)#posle teksta postavit ,
-    print("Нечётных цифр:",paaritu)
+    print("Чётных цифр:", paaris)#posle teksta postavit , 
+    print("Нечётных цифр:", paaritu)
     print()
 #''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     print("*Переворачиваем* введённое число")
     print()
     b=0
-    while a > 0:#posle while nado ispolzovat :
+    while a > 0:
         number = a % 10
         a = a // 10
         b = b * 10
-        b =+ number#pravilno podstavit pod while
+        b += number
     print("*Перевёрнутое* число", b)
     print()
 #''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-    print("Проверяем гипотезу Сиракуз")#lisnaja (
+    print("Проверяем гипотезу Сиракуз")
     print()
-    if c % 2 == 0:#dobavit ese odno =
+    if c % 2 == 0:#dobavit ese odno =, stobi proverat ravenstvo
         print("с - чётное число. Делим на 2.")
     else:
         print("с - нечётное число. Умножаем на 3, прибавляем 1 и делим на 2.")
     while c != 1:
-        if c % 2 == 0:#dobavit ese odno =
-            c == c / 2
+        if c % 2 == 0:#dobavit ese odno =, stobi proverat ravenstvo
+            c = c / 2
         else:
-            c == (3*c + 1) / 2
-            print(c, end=" ")#zakrit "
+            c = (3*c + 1) / 2
+            print(round(c), end=" ")
     print()
-    print("Гипотеза верна")#ne te " bili ispolzovani
+    print("Гипотеза верна")
